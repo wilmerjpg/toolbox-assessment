f85d3df363fb4ed97edfa0f43bb0a8a644078ab4
Wilmer Prieto <wilmerprieto3000@gmail.com>
2023-04-07 20:42:28 -0400

    feat(toolbox-api): use util to get valid records

--- toolbox-api/src/services/files.js
+++ toolbox-api/src/services/files.js
@@ -1,6 +1,5 @@
-import { parse } from 'csv-parse/sync';
 import { getFileData, getFiles } from '../lib/files-external-api.js';
-import { isValidRecord } from '../utils.js';
+import { getValidRecordsFromCSV } from '../utils.js';
 
 const getFilesData = async () => {
   const { files = [] } = await getFiles();
@@ -9,17 +8,8 @@ const getFilesData = async () => {
   for (let i = 0; i < files.length; i++) {
     try {
       const file = await getFileData(files[i]);
-      const records = parse(file, {
-        columns: true,
-        skip_empty_lines: true,
-        relaxColumnCount: true,
-      });
-
-      records.forEach((record) => {
-        if (isValidRecord(record)) {
-          filesData.push(record);
-        }
-      });
+      const records = getValidRecordsFromCSV(file);
+      filesData.push(...records);
     } catch (error) {
       console.error(`Error processing ${files[i]} ${error?.message}`);
     }
